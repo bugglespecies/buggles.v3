04fe25092c153acfc98e9bf9f44a43aaa5a9338a
YourNetworkNerd <radio.dark@outlook.com>
2019-06-24 03:32:42 -0400

    Removed all current modules

---
+++ index.js
@@ -0,0 +1,14 @@
+const express       = require('express');
+const exprSession   = require('express-session');
+const passport      = require('passport');
+const sDiscord      = require('passport-discord');
+const bodyParse     = require('body-parser');
+const helmet        = require('helmet');
+const flash         = require('req-flash');
+const ioSockets     = require('socket.io');
+const method        = require('method-override');
+
+const appCore       = express();
+const serverCore    = http.createServer(appCore);
+const io            = ioSockets(serverCore);
+
