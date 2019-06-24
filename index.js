33f476a4437cbe9b568fdd9b29b412f0baf82d2b
YourNetworkNerd <radio.dark@outlook.com>
2019-06-24 03:56:55 -0400

    Changed server METHODS

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express       = require('express');
-const exprSession   = require('express-session');
-const passport      = require('passport');
-const sDiscord      = require('passport-discord');
-const bodyParse     = require('body-parser');
-const helmet        = require('helmet');
-const flash         = require('req-flash');
-const ioSockets     = require('socket.io');
-const method        = require('method-override');
+const express           = require('express');
+const exprSession       = require('express-session');
+const passport          = require('passport');
+const sDiscord          = require('passport-discord');
+const bodyParse         = require('body-parser');
+const helmet            = require('helmet');
+const flash             = require('req-flash');
+const ioSockets         = require('socket.io');
+const methodOverride    = require('method-override');
 
-const appCore       = express();
-const serverCore    = http.createServer(appCore);
-const io            = ioSockets(serverCore);
+const appCore           = express();
+const serverCore        = http.createServer(appCore);
+const io                = ioSockets(serverCore);
 
 appCore.use(helmet());
 appCore.use(flash());
 appCore.use(bodyParse.urlencoded({ extended: true }));
+appCore.use(methodOverride('_method'));
 appCore.set('view engine', 'ejs');
